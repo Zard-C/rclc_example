efbac5e9e8b5fbabedafa8bd984c401a52a5c9f4
Zard-C <patrick.zhang5233@gmail.com>
2024-03-29 01:41:33 +0800

    update: apply code formatting
    Signed-off-by: Zard-C <patrick.zhang5233@gmail.com>

--- src/pub_loaned_msg.c
+++ src/pub_loaned_msg.c
@@ -7,112 +7,126 @@ rcl_publisher_t my_pub;
 rcl_timer_t my_timer;
 const rosidl_message_type_support_t *my_type_support = NULL;
 
-void timer_callback(rcl_timer_t *timer, int64_t last_call_time) {
-  (void)last_call_time;
-  (void)timer;
+void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
+{
+    (void)last_call_time;
+    (void)timer;
 
-  bool ret = rcl_publisher_can_loan_messages(&my_pub);
-  if (ret) {
-    rmw_publisher_t *rmw_publisher = rcl_publisher_get_rmw_handle(&my_pub);
-    void *loaned_msg = NULL;
-    rcl_ret_t rc =
-        rmw_borrow_loaned_message(rmw_publisher, my_type_support, &loaned_msg);
+    bool ret = rcl_publisher_can_loan_messages(&my_pub);
+    if (ret)
+    {
+        rmw_publisher_t *rmw_publisher = rcl_publisher_get_rmw_handle(&my_pub);
+        void *loaned_msg = NULL;
+        rcl_ret_t rc = rmw_borrow_loaned_message(rmw_publisher, my_type_support, &loaned_msg);
 
-    if (rc == RCL_RET_OK && loaned_msg != NULL) {
-      RCUTILS_LOG_INFO("Loan message from rmw success.\n");
-    } else {
-      RCUTILS_LOG_ERROR("Loan message from rmw failed\n");
+        if (rc == RCL_RET_OK && loaned_msg != NULL)
+        {
+            RCUTILS_LOG_INFO("Loan message from rmw success.\n");
+        }
+        else
+        {
+            RCUTILS_LOG_ERROR("Loan message from rmw failed\n");
+        }
+        ((std_msgs__msg__Float32 *)loaned_msg)->data = 1.0;
+        // This pub function will return the loaned message to rmw.
+        rc = rcl_publish_loaned_message(&my_pub, loaned_msg, NULL);
+        if (rc != RCL_RET_OK)
+        {
+            RCUTILS_LOG_ERROR("Error rcl_publish_loaned_message\n");
+        }
+        else
+        {
+            RCUTILS_LOG_INFO("Publish Loaned Message Success!\n");
+        }
     }
-    ((std_msgs__msg__Float32 *)loaned_msg)->data = 1.0;
-    // This pub function will return the loaned message to rmw.
-    rc = rcl_publish_loaned_message(&my_pub, loaned_msg, NULL);
-    if (rc != RCL_RET_OK) {
-      RCUTILS_LOG_ERROR("Error rcl_publish_loaned_message\n");
-    } else {
-      RCUTILS_LOG_INFO("Publish Loaned Message Success!\n");
-    }
-  }
 }
 
-int main(int argc, const char *const *argv) {
-  rcl_allocator_t allocator = rcl_get_default_allocator();
-  rclc_support_t support;
-  rcl_ret_t rc;
+int main(int argc, const char *const *argv)
+{
+    rcl_allocator_t allocator = rcl_get_default_allocator();
+    rclc_support_t support;
+    rcl_ret_t rc;
 
-  // create init options
-  rc = rclc_support_init(&support, argc, argv, &allocator);
-  if (rc != RCL_RET_OK) {
-    printf("Error rclc_support_init\n");
-    return -1;
-  }
+    // create init options
+    rc = rclc_support_init(&support, argc, argv, &allocator);
+    if (rc != RCL_RET_OK)
+    {
+        printf("Error rclc_support_init\n");
+        return -1;
+    }
 
-  // create rcl_node
-  rcl_node_t my_node = rcl_get_zero_initialized_node();
-  rc = rclc_node_init_default(&my_node, "node_pub_loan", "", &support);
-  if (rc != RCL_RET_OK) {
-    printf("Error in rclc_node_init_default\n");
-    return -1;
-  }
+    // create rcl_node
+    rcl_node_t my_node = rcl_get_zero_initialized_node();
+    rc = rclc_node_init_default(&my_node, "node_pub_loan", "", &support);
+    if (rc != RCL_RET_OK)
+    {
+        printf("Error in rclc_node_init_default\n");
+        return -1;
+    }
 
-  const char *topic_name = "TEST_LOANED_TOPIC";
-  my_type_support = ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32);
+    const char *topic_name = "TEST_LOANED_TOPIC";
+    my_type_support = ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32);
 
-  if (my_type_support == NULL) {
-    printf("Error in ROSIDL_GET_MSG_TYPE_SUPPORT\n");
-    return -1;
-  }
+    if (my_type_support == NULL)
+    {
+        printf("Error in ROSIDL_GET_MSG_TYPE_SUPPORT\n");
+        return -1;
+    }
 
-  rc = rclc_publisher_init_default(&my_pub, &my_node, my_type_support,
-                                   topic_name);
+    rc = rclc_publisher_init_default(&my_pub, &my_node, my_type_support, topic_name);
 
-  if (RCL_RET_OK != rc) {
-    printf("Error in rclc_publisher_init_default %s.\n", topic_name);
-    return -1;
-  }
+    if (RCL_RET_OK != rc)
+    {
+        printf("Error in rclc_publisher_init_default %s.\n", topic_name);
+        return -1;
+    }
 
-  // wait for dds to finish discovery
-  rclc_sleep_ms(1000);
+    // wait for dds to finish discovery
+    rclc_sleep_ms(1000);
 
-  // create timer,
-  rcl_timer_t my_timer = rcl_get_zero_initialized_timer();
-  const unsigned int timer_timeout = 1000;
-  rc = rclc_timer_init_default(&my_timer, &support, RCL_MS_TO_NS(timer_timeout),
-                               timer_callback);
-  if (rc != RCL_RET_OK) {
-    printf("Error in rclc_timer_init_default.\n");
-    return -1;
-  }
+    // create timer,
+    rcl_timer_t my_timer = rcl_get_zero_initialized_timer();
+    const unsigned int timer_timeout = 1000;
+    rc = rclc_timer_init_default(&my_timer, &support, RCL_MS_TO_NS(timer_timeout), timer_callback);
+    if (rc != RCL_RET_OK)
+    {
+        printf("Error in rclc_timer_init_default.\n");
+        return -1;
+    }
 
-  // create executor
-  rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
-  unsigned int num_handles = 1;
-  rc = rclc_executor_init(&executor, &support.context, num_handles, &allocator);
-  if (rc != RCL_RET_OK) {
-    printf("Error in rclc_executor_init.\n");
-    return -1;
-  }
+    // create executor
+    rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
+    unsigned int num_handles = 1;
+    rc = rclc_executor_init(&executor, &support.context, num_handles, &allocator);
+    if (rc != RCL_RET_OK)
+    {
+        printf("Error in rclc_executor_init.\n");
+        return -1;
+    }
 
-  // add timer to executor
-  rc = rclc_executor_add_timer(&executor, &my_timer);
-  if (rc != RCL_RET_OK) {
-    printf("Error in rclc_executor_add_timer.\n");
-    return -1;
-  }
+    // add timer to executor
+    rc = rclc_executor_add_timer(&executor, &my_timer);
+    if (rc != RCL_RET_OK)
+    {
+        printf("Error in rclc_executor_add_timer.\n");
+        return -1;
+    }
 
-  // spin executor
-  rclc_executor_spin(&executor);
+    // spin executor
+    rclc_executor_spin(&executor);
 
-  // clean up
-  rc = rclc_executor_fini(&executor);
-  rc += rcl_timer_fini(&my_timer);
-  rc += rcl_publisher_fini(&my_pub, &my_node);
-  rc += rcl_node_fini(&my_node);
-  rc += rclc_support_fini(&support);
+    // clean up
+    rc = rclc_executor_fini(&executor);
+    rc += rcl_timer_fini(&my_timer);
+    rc += rcl_publisher_fini(&my_pub, &my_node);
+    rc += rcl_node_fini(&my_node);
+    rc += rclc_support_fini(&support);
 
-  if (rc != RCL_RET_OK) {
-    printf("Error while cleaning up\n");
-    return -1;
-  }
+    if (rc != RCL_RET_OK)
+    {
+        printf("Error while cleaning up\n");
+        return -1;
+    }
 
-  return 0;
+    return 0;
 }
--- src/timer_with_context.c
+++ src/timer_with_context.c
@@ -6,100 +6,112 @@
 typedef void (*timer_callback_t)(void *context);
 
 /* customized timer_context_t */
-typedef struct demo_timer_context_s {
-  rcl_timer_t timer;
-  timer_callback_t callback; /* for calling  customized timer_callback */
-  void *context;             /* for passing customized context */
-  uint64_t period;
+typedef struct demo_timer_context_s
+{
+    rcl_timer_t timer;
+    timer_callback_t callback; /* for calling  customized timer_callback */
+    void *context;             /* for passing customized context */
+    uint64_t period;
 } demo_timer_context_t;
 
 /* callback function to regist */
-static void my_timer_callback(rcl_timer_t *timer, int64_t last_call_time) {
-  RCLC_UNUSED(last_call_time);
-  if (timer != NULL) {
-    demo_timer_context_t *timer_context = (demo_timer_context_t *)timer;
-    if (timer_context != NULL) {
-      timer_context->callback(timer_context->context);
+static void my_timer_callback(rcl_timer_t *timer, int64_t last_call_time)
+{
+    RCLC_UNUSED(last_call_time);
+    if (timer != NULL)
+    {
+        demo_timer_context_t *timer_context = (demo_timer_context_t *)timer;
+        if (timer_context != NULL)
+        {
+            timer_context->callback(timer_context->context);
+        }
     }
-  }
 }
 
-static void custom_timer_callback(void *context) {
-  int *counter = (int *)context;
-  printf("custom_timer_callback: %d\n", *counter);
-  (*counter)++;
+static void custom_timer_callback(void *context)
+{
+    int *counter = (int *)context;
+    printf("custom_timer_callback: %d\n", *counter);
+    (*counter)++;
 }
 
-int main(int argc, const char *const *argv) {
-  rcl_allocator_t allocator = rcl_get_default_allocator();
-  rclc_support_t support;
-  rcl_ret_t rc;
+int main(int argc, const char *const *argv)
+{
+    rcl_allocator_t allocator = rcl_get_default_allocator();
+    rclc_support_t support;
+    rcl_ret_t rc;
 
-  // create init_options
-  rc = rclc_support_init(&support, argc, argv, &allocator);
-  if (rc != RCL_RET_OK) {
-    printf("Error rclc_support_init.\n");
-    return -1;
-  }
+    // create init_options
+    rc = rclc_support_init(&support, argc, argv, &allocator);
+    if (rc != RCL_RET_OK)
+    {
+        printf("Error rclc_support_init.\n");
+        return -1;
+    }
 
-  // create rcl_node
-  rcl_node_t my_node = rcl_get_zero_initialized_node();
-  rc = rclc_node_init_default(&my_node, "node_0", "", &support);
-  if (rc != RCL_RET_OK) {
-    printf("Error in rclc_node_init_default\n");
-    return -1;
-  }
+    // create rcl_node
+    rcl_node_t my_node = rcl_get_zero_initialized_node();
+    rc = rclc_node_init_default(&my_node, "node_0", "", &support);
+    if (rc != RCL_RET_OK)
+    {
+        printf("Error in rclc_node_init_default\n");
+        return -1;
+    }
 
-  int count = 0;
+    int count = 0;
 
-  // create a customized timer_context_t
-  demo_timer_context_t timer_context;
-  timer_context.timer = rcl_get_zero_initialized_timer();
-  timer_context.period = 1000;
-  timer_context.callback = custom_timer_callback;
-  timer_context.context = &count;
+    // create a customized timer_context_t
+    demo_timer_context_t timer_context;
+    timer_context.timer = rcl_get_zero_initialized_timer();
+    timer_context.period = 1000;
+    timer_context.callback = custom_timer_callback;
+    timer_context.context = &count;
 
-  // create a timer, which will call the custom callback function with period=
-  // `timer_timeout` ms
-  rc = rclc_timer_init_default(&timer_context.timer, &support,
-                               RCL_MS_TO_NS(timer_context.period),
-                               my_timer_callback);
+    // create a timer, which will call the custom callback function with period=
+    // `timer_timeout` ms
+    rc = rclc_timer_init_default(&timer_context.timer, &support, RCL_MS_TO_NS(timer_context.period), my_timer_callback);
 
-  if (rc != RCL_RET_OK) {
-    printf("Error in rcl_timer_init_default.\n");
-    return -1;
-  } else {
-    printf("timer initialized with period %lu ms\n", timer_context.period);
-  }
+    if (rc != RCL_RET_OK)
+    {
+        printf("Error in rcl_timer_init_default.\n");
+        return -1;
+    }
+    else
+    {
+        printf("timer initialized with period %lu ms\n", timer_context.period);
+    }
 
-  // add timer to executor, since we only have one callback function.
-  size_t num_handles = 1;
-  rclc_executor_t executor;
-  rc = rclc_executor_init(&executor, &support.context, num_handles, &allocator);
-  if (rc != RCL_RET_OK) {
-    printf("Error in rclc_executor_init.\n");
-    return -1;
-  }
+    // add timer to executor, since we only have one callback function.
+    size_t num_handles = 1;
+    rclc_executor_t executor;
+    rc = rclc_executor_init(&executor, &support.context, num_handles, &allocator);
+    if (rc != RCL_RET_OK)
+    {
+        printf("Error in rclc_executor_init.\n");
+        return -1;
+    }
 
-  rc = rclc_executor_add_timer(&executor, &timer_context.timer);
-  if (rc != RCL_RET_OK) {
-    printf("Error in rclc_executor_add_timer.\n");
-    return -1;
-  }
+    rc = rclc_executor_add_timer(&executor, &timer_context.timer);
+    if (rc != RCL_RET_OK)
+    {
+        printf("Error in rclc_executor_add_timer.\n");
+        return -1;
+    }
 
-  // spin the node
-  rclc_executor_spin(&executor);
+    // spin the node
+    rclc_executor_spin(&executor);
 
-  // clean up
-  rc = rclc_executor_fini(&executor);
-  rc += rcl_timer_fini(&timer_context.timer);
-  rc += rcl_node_fini(&my_node);
-  rc += rclc_support_fini(&support);
+    // clean up
+    rc = rclc_executor_fini(&executor);
+    rc += rcl_timer_fini(&timer_context.timer);
+    rc += rcl_node_fini(&my_node);
+    rc += rclc_support_fini(&support);
 
-  if (rc != RCL_RET_OK) {
-    printf("Error while cleaning up!\n");
-    return -1;
-  }
+    if (rc != RCL_RET_OK)
+    {
+        printf("Error while cleaning up!\n");
+        return -1;
+    }
 
-  return 0;
+    return 0;
 }
